4885db1fb2282d8aee082a9ad64130b0dadf38cd
Anca Simon <ancarsimon@gmail.com>
2020-03-07 09:15:39 -0600

    added fixes to numbers

--- src/javascripts/components/play.js
+++ src/javascripts/components/play.js
@@ -4,7 +4,7 @@ import utils from '../helpers/utils';
 const buildPlayComponent = () => {
   let domString = '<h2>PLAY</h2>';
   domString += '<h3>Rosie</h3>';
-  domString += `<p>${playData.getFunScore}</p>`;
+  domString += `<p>${playData.getFunScore()}</p>`;
   domString += '<button id="superFun">Super Fun Activity</button>';
   domString += '<button id="slightlyFun">Just Slightly Fun Activity</button>';
   utils.printToDom('play', domString);
